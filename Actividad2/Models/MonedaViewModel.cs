40b8d6137eb917789a1ce3af31cd5548dd77ed7e
EnriqueSM-30 <enriquesanmiguel32@gmail.com>
2023-09-28 15:05:17 -0600

    Agregar archivos de proyecto.

---
+++ Actividad2/Models/MonedaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Actividad2.Models
+{
+    public class MonedaViewModel
+    {
+        public string? TipoMoneda { get; set; }
+        public decimal MonedaOrigen { get; set; }
+        public decimal MonedaDestino { get; set; }
+    }
+}
